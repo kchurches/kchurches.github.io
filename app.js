f6c561c603d7a7725db89d9478549f319ba5bc91
kchurches <81184392+kchurches@users.noreply.github.com>
2021-05-05 10:35:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.nav-menu');
+const mobileMenu = document.querySelector('#mobile-menu');
+const navMenu = document.querySelector('.nav-menu');
+const header = document.querySelector('header');
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+// On hamburger menu click, turn hamburger menu into X, open nav menu, and add or remove 'box-shadow' class to the header depending on status
+mobileMenu.addEventListener('click', function() {
+    mobileMenu.classList.toggle('is-active');
+    navMenu.classList.toggle('active');
+
+    if (header.classList === 'box-shadow') {
+        header.classList.remove('box-shadow');
+    } else {
+        header.classList.toggle('box-shadow');
+    };
+});
+
+// On nav menu click, turn X back into hamburger menu, close nav menu, and add 'box-shadow' class to header
+navMenu.addEventListener('click', function() {
+    mobileMenu.classList.remove('is-active');
+    navMenu.classList.remove('active');
+    header.classList.toggle('box-shadow');
 });
