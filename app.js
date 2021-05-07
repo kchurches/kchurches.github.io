96a9efa3b372a73483f1bb8a0d8d7afabecf8b67
kchurches <81184392+kchurches@users.noreply.github.com>
2021-05-07 14:19:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,21 @@
-const mediaQuery = window.matchMedia('(max-width: 768px)');
 const mobileMenu = document.querySelector('#mobile-menu');
 const navMenu = document.querySelector('.nav-menu');
 const header = document.querySelector('header');
 
 
-mediaQuery.addEventListener('change', handleDeviceChange);
+mobileMenu.addEventListener('click', function() { // and the hamburger is clicked,
+    mobileMenu.classList.toggle('is-active'); // the hamburger turns into an X,
+    navMenu.classList.toggle('active'); // the navMenu slides out from the left,
 
+    if (header.classList === 'box-shadow') { // and IF the header has a shadow (which it does on page load),
+        header.classList.remove('box-shadow'); // remove the shadow on the header,
+    } else {
+        header.classList.toggle('box-shadow'); // otherwise (on second click), leave the shadow on the header alone.
+    }
+});
 
-function handleDeviceChange(e) {
-    if (e.matches) { // If the screen is 768px or smaller,
-        mobileMenu.addEventListener('click', function() { // and the hamburger is clicked,
-            mobileMenu.classList.toggle('is-active'); // the hamburger turns into an X,
-            navMenu.classList.toggle('active'); // the navMenu slides out from the left,
-        
-            if (header.classList === 'box-shadow') { // and IF the header has a shadow (which it does on page load),
-                header.classList.remove('box-shadow'); // remove the shadow on the header,
-            } else {
-                header.classList.toggle('box-shadow'); // otherwise (on second click), leave the shadow on the header alone.
-        }});
-
-        navMenu.addEventListener('click', function() { // and a navMenu item is clicked,
-            mobileMenu.classList.remove('is-active'); // change the X back into a hamburger,
-            navMenu.classList.remove('active'); // slide the navMenu off screen to the left,
-            header.classList.add('box-shadow'); // and put the shadow back on the header.
-        });
-    } else { // Otherwise (if the screen is 769px or bigger),
-        navMenu.addEventListener('click', function() { // and a navMenu item is clicked,
-            header.classList.add('box-shadow'); // add the shadow to the header.
-        });
-    };
-};
-
-
-handleDeviceChange(mediaQuery);
+navMenu.addEventListener('click', function() { // and a navMenu item is clicked,
+    mobileMenu.classList.remove('is-active'); // change the X back into a hamburger,
+    navMenu.classList.remove('active'); // slide the navMenu off screen to the left,
+    header.classList.add('box-shadow'); // and put the shadow back on the header.
+});
